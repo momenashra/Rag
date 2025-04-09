7b0b30f3e64341b145251b3b51f19b571915f909
momenashra <momanpipo55@gmail.com>
2025-04-09 18:24:52 +0200

    adding chunking ability

---
+++ src/controllers/ProcessController.py
@@ -0,0 +1,59 @@
+from fastapi import UploadFile
+from .BaseController import BaseController
+from .ProjectController import ProjectController
+from models import ResponseSignals
+import os
+from langchain_community.document_loaders import TextLoader 
+from langchain_community.document_loaders import PyMuPDFLoader
+from models import ProcessingEnums
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+class ProcessController(BaseController):
+    def __init__(self,project_id : str):
+        super().__init__()
+        self.project_id=project_id
+        self.project_path=ProjectController().get_project_path(project_id=project_id)   
+    
+    def get_file_extension(self,file_id:str):
+        return os.path.splitext(file_id)[-1]
+    
+    def get_file_loader(self,file_id:str):
+        self.file_ext=self.get_file_extension(file_id=file_id)
+        file_path = os.path.join (
+             self.project_path,
+            file_id
+        )
+        if self.file_ext == ProcessingEnums.PDF.value:
+            return PyMuPDFLoader(file_path,encoding="utf-8")
+        if self.file_ext == ProcessingEnums.TXT.value:
+            return TextLoader(file_path)
+        return None        
+        
+    def get_file_content(self,file_id:str):
+
+        loader=self.get_file_loader(file_id=file_id)
+        return loader.load() #return content and metadata
+    
+    def process_file_content(self,file_content:list,file_id:str,chunk_size : int =100,overlap_size:int = 20):
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            # Set a really small chunk size, just to show.
+            chunk_size=chunk_size,
+            chunk_overlap=overlap_size,
+            length_function=len,
+            is_separator_regex=False,
+        )
+
+        file_content_text=[
+            rec.page_content
+            for rec in file_content 
+        ]
+        file_content_metadata=[
+            rec.metadata
+            for rec in file_content 
+        ]
+        chunks=text_splitter.create_documents(
+            file_content_text,
+            metadatas=file_content_metadata
+        )
+
+        return chunks
