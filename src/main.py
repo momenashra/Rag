3a0c254e818541c5efbdbae6c56e625b3dd34117
momenashra <momanpipo55@gmail.com>
2025-04-17 23:34:00 +0200

    add accessability to LLM factory

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from routes import data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 
+from stores import LLMProviderFactory
+from stores.llm.LLMEnums import LLMEnums
+
 app = FastAPI()
 
 
@@ -12,7 +15,16 @@ async def startup_db_clint():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.mongo_db = app.mongo_conn[settings.MONGODB_DB]
+    
+    llm_provider_factory = LLMProviderFactory(settings)
 
+    #generation client
+    app.generation_client = llm_provider_factory.create(provider_name=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODLE_ID)
+
+    #embedding client
+    app.embedding_client = llm_provider_factory.create(provider_name=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODLE_ID,embedding_size=settings.EMBEDDING_SIZE)
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
