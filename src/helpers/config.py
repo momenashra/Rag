eca3006576b9edb9ea9a6048d2d494abc16ce040
momenashra <momanpipo55@gmail.com>
2025-04-17 23:34:17 +0200

    add configs for LLM factory

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -11,6 +11,20 @@ class Settings(BaseSettings):
     MONGODB_URL: str
     MONGODB_DB: str
 
+    GENERATION_BACKEND:str
+    EMBEDDING_BACKEND:str
+
+    OPENAI_API_KEY:str = None
+    OPENAI_API_URL:str = None
+    COHERE_API_KEY:str = None
+
+    GENERATION_MODLE_ID:str = None
+    EMBEDDING_MODLE_ID:str = None
+    EMBEDDING_MODLE_SIZE:int = None
+
+    INPUT_DEFAULT_MAX_CARACTERS:int = None    
+    GENERATION_DEFAULT_MAX_TOKENS:int = None
+    GENERATION_DEFAULT_TEMPRATURE:float = None
     class Config:
         env_file = ".env"
 
