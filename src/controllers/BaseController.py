27e94f4c682515cf922dd5b4ff95e49e5554e62d
momenashra <momanpipo55@gmail.com>
2025-04-18 19:20:03 +0200

    implement vector database interface and Qdrant provider; add database path handling and enums

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -10,6 +10,14 @@ class BaseController:
 
         self.base_dir = os.path.dirname(os.path.dirname(__file__))
         self.file_dir = os.path.join(self.base_dir, "assets/files")
+        self.db_dir = os.path.join(self.base_dir, "assets/database")
 
     def generate_random_string(self, length: int = 12):
         return "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
+    def get_database_path(self,db_name:str):
+        db_path= os.path.join(self.db_dir, db_name)
+
+        if not os.path.exists(db_path):
+            os.makedirs(db_path)
+        return db_path
---
+++ src/stores/vectordb/VectorDBProvidersFactory.py
@@ -0,0 +1,17 @@
+from .VectorDBEnums import VectorDBEnums,DistanceTypeEnum
+from .providers import QdrantDBProvider
+from controllers.BaseController import BaseController
+
+class VectorDBProvidersFactory:
+    def __init__(self,config: dict ):
+        self.config = config
+        self.base_controller = BaseController()
+    
+    def create(self,provider_name:str):
+        if provider_name == VectorDBEnums.QDRANT.value:
+            return QdrantDBProvider (
+                db_path=self.base_controller.get_database_path(db_name=self.config.VECTOR_DB_PATH),
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+            )
+
+        return None
