0870b006d0e2b623dd6aaf8fe8379a36f91f090f
momenashra <momanpipo55@gmail.com>
2025-05-04 19:56:19 +0300

    handling

--- docker/rag/entrypoint.sh
+++ docker/rag/entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 echo "Running database migrations..."
-cd /app/models/db_schemes/rag/
+cd /app/models/db_shemas/rag/   
 alembic upgrade head
 cd /app
+echo "Starting FastAPI application..."
+exec "$@"
